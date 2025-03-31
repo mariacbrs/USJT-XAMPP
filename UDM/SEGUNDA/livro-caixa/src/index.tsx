e24869212fc7b3a4fddc8bd07ea08b914389d0fa
Maria Clara Borges Rodrigues Santos <mariacbrsantos@gmail.com>
2025-03-31 12:55:51 -0300

    updates

--- UDM/SEGUNDA/livro-caixa/src/index.tsx
+++ UDM/SEGUNDA/livro-caixa/src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+// src/index.tsx
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'; import React from 'react';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
