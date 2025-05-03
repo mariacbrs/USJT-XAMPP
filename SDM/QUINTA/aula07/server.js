3a7aecd01f8f4cdbdfaa8a5f77656b87fbac8945
Maria Clara Borges Rodrigues Santos <mariacbrsantos@gmail.com>
2025-05-03 12:24:06 -0300

    exercicioCliente

--- SDM/QUINTA/aula07/server.js
+++ SDM/QUINTA/aula07/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
-const conn = knex({
+const connLoja = knex({
     client: "mysql2",
     connection: {
         host: "localhost",
@@ -20,19 +20,30 @@ const conn = knex({
     }
 });
 
+const connCliente = knex({
+    client: "mysql2",
+    connection: {
+        host: "localhost",
+        user: "root",
+        password: "mcbrs",
+        database: "cliente"
+    }
+});
+
 app.get("/", (req, res) => {
     res.json({ resposta: "Welcome to my store" })
 });
 
 app.get("/produtos", (req, res, next) => {
-    conn("produto")
+    connLoja("produto")
+        .orderBy("id")
         .then(dados => res.json(dados))
         .catch(next);
 });
 
 app.get("/produtos/:idProd", (req, res, next) => {
     const id = req.params.idProd;
-    conn("produto")
+    connLoja("produto")
         .where("id", id)
         .first()
         .then(dados => {
@@ -45,7 +56,7 @@ app.get("/produtos/:idProd", (req, res, next) => {
 });
 
 app.post("/produtos", (req, res, next) => {
-    conn("produto")
+    connLoja("produto")
         .insert(req.body)
         .then(dados => {
             if (!dados) {
@@ -62,7 +73,7 @@ app.post("/produtos", (req, res, next) => {
 
 app.put("/produtos/:idProd", (req, res, next) => {
     const id = req.params.idProd;
-    conn("produto")
+    connLoja("produto")
         .where("id", id)
         .update(req.body)
         .then(dados => {
@@ -79,7 +90,7 @@ app.put("/produtos/:idProd", (req, res, next) => {
 
 app.delete("/produtos/:idProd", (req, res, next) => {
     const id = req.params.idProd;
-    conn("produto")
+    connLoja("produto")
         .where("id", id)
         .delete()
         .then(dados => {
@@ -102,15 +113,67 @@ app.use((err, req, res, next) => {
         })
 })
 
-//Exercício: Criar no banco de dados a tabela cliente,
-// se ela ainda não existe,
-// que deve conter as colunas id, nome, altura
-// criar um endpoint para:
+//Exercício: Criar no banco de dados a tabela cliente, se ela ainda não existe, que deve conter as colunas id, nome, altura criar um endpoint para:
 //  -> retornar os clientes ordenados pela altura
-//  -> inserir um novo cliente
-//  -> editar um cliente
-//  -> excluir um cliente
+app.get("/clientes", (req, res, next) => {
+    connCliente("pessoa")
+        .orderBy("altura")
+        .then(dados => res.json(dados))
+        .catch(next);
+});
 
+//  -> inserir um novo cliente
+app.post("/clientes", (req, res, next) => {
+    connCliente("pessoa")
+        .insert(req.body)
+        .then(dados => {
+            if (!dados) {
+                return next(errors(400, "It was not possible to add new person"))
+            }
+            res.status(201).json({
+                resposta: "Person was added",
+                id: dados[0]
+            });
+            //res.status(201).json( dados )
+        })
+        .catch(next);
+});
+
+//  -> editar um cliente
+app.put("/clientes/:idPessoa", (req, res, next) => {
+    const id = req.params.idPessoa;
+    connCliente("pessoa")
+        .where("id", id)
+        .update(req.body)
+        .then(dados => {
+            if (!dados) {
+                return next(errors(404, "Person was not found"))
+            }
+            res.status(200).json({
+                resposta: "Person was edited",
+                dados: dados
+            });
+        })
+        .catch(next);
+});
+
+//  -> excluir um cliente
+app.delete("/clientes/:idPessoa", (req, res, next) => {
+    const id = req.params.idPessoa;
+    connCliente("pessoa")
+        .where("id", id)
+        .delete()
+        .then(dados => {
+            if (!dados) {
+                return next(errors(404, "Product was not found"))
+            }
+            res.status(200).json({
+                resposta: "Product was deleted",
+                dados: dados
+            });
+        })
+        .catch(next);
+});
 
 app.listen(PORT, () => {
     console.log(`Store is being executed in http://localhost:${PORT}`)
